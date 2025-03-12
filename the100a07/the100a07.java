c5a481972d4dc3f8ce49920630cf6d0b9f910030
honlllxx <2656468910@qq.com>
2025-03-12 12:13:04 +0800

    thanIndex

--- the100a07/the100a07.java
+++ the100a07/the100a07.java
@@ -11,6 +11,7 @@ public class the100a07 {
         int left = 1;
         int right = height.length - 2;
         // 使用双指针法，当 left 小于 right 时继续循环
+        //这里是用本指针比较本指针值,终止条件为<=
         while (left <= right) {
             if (height[left - 1] < height[right + 1]) {
                 // 更新左侧最大高度
@@ -35,10 +36,31 @@ public class the100a07 {
         return sum;
     }
 
+ public static int trap2(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int ans = 0;
+        //这里是本指针位置比较下一位,所以<符号,<=循环中会超出范围
+        while(left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if(height[left] < height[right]) {
+                ans += leftMax - height[left];
+                left++;
+            } else {
+                ans += rightMax - height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+
    public static void main(String[] args) {
     System.out.println("========================================");
     int []arr={4,4,5,8,5,5,0};
-    System.out.println(trap(arr));
+    System.out.println(trap2(arr));
     System.out.println("========================================");
    }
 
